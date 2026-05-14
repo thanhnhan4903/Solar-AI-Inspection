89753f6ddbaaa0e8fc535f93fdf5b4e0da98489b
thanhnhan4903 <nltnhan04092003@gmail.com>
2026-05-15 01:35:07 +0700

    Gộp thành công code FE và fix conflict

--- datn_be/app/core/database.py
+++ datn_be/app/core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:123456789@localhost:3306/solar_ai_system"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:nhan4903@localhost:3306/solar_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
