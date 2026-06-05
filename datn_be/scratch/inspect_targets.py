720c95fbc8c9d07412ba67fe96e08c2c9314c168
thanhnhan4903 <nltnhan04092003@gmail.com>
2026-06-05 17:49:49 +0700

    clean

--- datn_be/scratch/inspect_targets.py
+++
@@ -1,7 +0,0 @@
-import json
-target_idxs = {25, 40, 46, 47, 52, 62, 65, 67, 68, 77, 78, 86}
-with open('data/results/debug_logs/DJI_0029_R_final_polygon_trace.jsonl', 'r') as f:
-    for line in f:
-        p = json.loads(line)
-        if p['raw_idx'] in target_idxs:
-            print(f"raw_idx: {p['raw_idx']}, string_id: {p.get('string_id')}, bbox: {p['bbox']}")
